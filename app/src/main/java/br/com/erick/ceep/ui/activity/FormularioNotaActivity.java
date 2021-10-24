8a38e85b765f2f40c6ccee98651c7e63e45cc875
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-24 18:54:26 -0300

    funcionalidade Salvar Notas no formulario adicionada

--- app/src/main/java/br/com/erick/ceep/ui/activity/FormularioNotaActivity.java
+++ app/src/main/java/br/com/erick/ceep/ui/activity/FormularioNotaActivity.java
@@ -1,11 +1,17 @@
 package br.com.erick.ceep.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import br.com.erick.ceep.R;
+import br.com.erick.ceep.dao.NotaDAO;
+import br.com.erick.ceep.model.Nota;
 
 public class FormularioNotaActivity extends AppCompatActivity {
 
@@ -19,5 +25,20 @@ public class FormularioNotaActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_formulario_nota_salva, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_formulario_nota_ic_salva){
+            EditText titulo = findViewById(R.id.formulario_nota_titulo);
+            EditText descricao = findViewById(R.id.formulario_nota_descricao);
+
+            Nota notaCriada = new Nota(titulo.getText().toString(), descricao.getText().toString());
+            new NotaDAO().insere(notaCriada);
+            finish();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
