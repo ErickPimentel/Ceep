dc4506b223aa70e5ed53d568c1f591ca455a9488
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2021-10-26 23:07:07 -0300

    cores e titulos adicionados | correcao do bug de quando voltar no formulario acontecia um crash do app

--- app/src/main/java/br/com/erick/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/erick/ceep/ui/activity/ListaNotasActivity.java
@@ -29,12 +29,16 @@ import br.com.erick.ceep.ui.recyclerview.helper.callback.NotaItemTouchHelperCall
 
 public class ListaNotasActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Notas";
     private ListaNotasAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+
+        setTitle(TITULO_APPBAR);
+
         List<Nota> todasNotas = pegaTodasNotas();
         configuraRecyclerView(todasNotas);
 
@@ -116,7 +120,7 @@ public class ListaNotasActivity extends AppCompatActivity {
     }
 
     private boolean temNota(@Nullable Intent data) {
-        return data.hasExtra(CHAVE_NOTA);
+        return data != null && data.hasExtra(CHAVE_NOTA);
     }
 
     private boolean resultadoOk(int resultCode) {
